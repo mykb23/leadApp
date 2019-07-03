58b31ee523702405d3510342169a61a3c71d06dc
Akinsuroju Michael <makinsuroju@gmail.com>
2019-07-03 20:49:21 +0100

    Added passport local auth strategy using email and password fields

---
+++ passport_setup.js
@@ -0,0 +1,57 @@
+let LocalStrategy = require('passport-local').Strategy;
+
+let bcrypt = require('bcrypt');
+let models = require('./db/models');
+
+const validPassport = function(user, password) {
+	return bcrypt.compareSync(password, user.password);
+};
+module.exports = function(passport) {
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
+	passport.deserializeUser(function(id, done) {
+		models.User.findOne({
+			where: {
+				id
+			}
+		}).then(user => {
+			if (user == null) {
+				done(new Error('Wrong user id.'));
+			}
+			done(null, user);
+		});
+	});
+	passport.use(
+		new LocalStrategy(
+			{
+				usernameField: 'email',
+				passwordField: 'password',
+				passReqToCallback: true
+			},
+			function(req, email, password, done) {
+				models.User.findOne({
+					where: {
+						email
+					}
+				})
+					.then(user => {
+						if (user == null) {
+							req.flash('message', 'Incorrect credentials');
+							return done(null, false);
+						} else if (user.passport == null || user.passport == undefined) {
+							req.flash('message', 'You must reset your password');
+							return done(null, false);
+						} else if (validPassport(user, password)) {
+							req.flash('message', 'Incorrect credentials');
+							return done(null, false);
+						}
+						return done(null, user);
+					})
+					.catch(err => {
+						done(err, false);
+					});
+			}
+		)
+	);
+};
