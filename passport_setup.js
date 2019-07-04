7ca72485ff85206f9423d4ccec812a239f36812f
Akinsuroju Michael <makinsuroju@gmail.com>
2019-07-04 19:24:20 +0100

    Implement hasAuth middleware and is_admin flag(Lead routesare protected by hasAuth and only the admin user has access to them)

--- passport_setup.js
+++ passport_setup.js
@@ -1,11 +1,13 @@
 let LocalStrategy = require('passport-local').Strategy;
 
 let bcrypt = require('bcrypt');
+// let { flash } = require('connect-flash');
 let models = require('./db/models');
 
-const validPassport = function(user, password) {
+const validPassword = function(user, password) {
 	return bcrypt.compareSync(password, user.password);
 };
+
 module.exports = function(passport) {
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
@@ -13,7 +15,7 @@ module.exports = function(passport) {
 	passport.deserializeUser(function(id, done) {
 		models.User.findOne({
 			where: {
-				id
+				id: id
 			}
 		}).then(user => {
 			if (user == null) {
@@ -22,35 +24,31 @@ module.exports = function(passport) {
 			done(null, user);
 		});
 	});
-	passport.use(
-		new LocalStrategy(
-			{
-				usernameField: 'email',
-				passwordField: 'password',
-				passReqToCallback: true
-			},
-			function(req, email, password, done) {
-				return models.User.findOne({
-					where: {
-						email
-					}
-				})
-					.then(user => {
-						if (user == null) {
-							req.flash('message', 'Incorrect credentials');
-							return done(null, false);
-						} else if (user.password == null || user.password == undefined) {
-							req.flash('message', 'You must reset your password');
-							return done(null, false);
-						} else if (validPassport(user, password)) {
-							req.flash('message', 'Incorrect credentials');
-							return done(null, false);
-						}
-						return done(null, user);
-					})
-					.catch(err => {
-						done(err, false);
-					});
+	passport.use(new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
+	},
+	function(req, email, password, done) {
+		return models.User.findOne({
+			where: {
+				email: email
+			}
+		}).then(user => {
+			if (user == null) {
+				req.flash('message', 'Incorrect credentials.');
+				return done(null, false);
+			} else if (user.password == null || user.password == undefined) {
+				req.flash('message', 'You must reset your password');
+				return done(null, false);
+			} else if(!validPassword(user, password)) {
+				req.flash('message', 'Incorrect credentials');
+				return done(null, false);
+			}
+			return done(null, user);
+		}).catch(err => {
+			done(err, false);
+		});
 			}
 		)
 	);
